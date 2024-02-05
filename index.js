ea205d1a48148c6118cdd0d576ca8acd013de6ec
Johannes Lepsius <jlepsius@gmail.com>
2024-02-05 16:29:24 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const translateToJavaScript = (inputFilePath, outputFilePath) => {
+  const content = fs.readFileSync(inputFilePath, 'utf-8');
+
+  const translatedCode = content
+    .replace(/fuckaround(\s\{\n[^]*?\n\s*\}\s*)findout\s\((\S*?)\)(\s\{\n[^]*?\n\s*\})/g, (match, tryBlock, errorVar, catchBlock) => {
+        return `try${tryBlock}catch (${errorVar})${catchBlock}`;
+    }).replace(/(?<!const|let)\syeet\s/gs, () => ' throw ');
+
+  fs.writeFileSync(path.join(__dirname, 'build', outputFilePath), translatedCode, 'utf-8');
+}
+
+translateToJavaScript(process.argv[2], 'output.js');
